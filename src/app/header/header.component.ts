7ed82dca64521df8f8cc4c516b30f9038eb6d61b
Alex-Mac <alex.hongquan.xiao@gmail.com>
2018-04-15 20:36:22 -0700

    add header selection

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('home') homeSelection: ElementRef;
+
+  currentSelection: any;
+  currentHover: any;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+
+    this.currentSelection = this.homeSelection.nativeElement;
+
+    this.renderer.setStyle(this.currentSelection, 'border-bottom', '5px solid #5c8bff');
+    this.renderer.setStyle(this.currentSelection, 'line-height', '35px');
+    this.renderer.setStyle(this.currentSelection, 'height', '35px');
+  }
+
+  selectMenu(selection: string, event: Event) {
+
+    if (event.target !== this.currentSelection) {
+
+      if (this.currentSelection) {
+        this.renderer.setStyle(this.currentSelection, 'border-bottom', 'none');
+        this.renderer.setStyle(this.currentSelection, 'line-height', '40px');
+        this.renderer.setStyle(this.currentSelection, 'height', '40px');
+      }
+
+      this.currentSelection = event.target;
+      this.renderer.setStyle(this.currentSelection, 'border-bottom', '5px solid #5c8bff');
+      this.renderer.setStyle(this.currentSelection, 'line-height', '35px');
+      this.renderer.setStyle(this.currentSelection, 'height', '35px');
+
+      switch (selection) {
+        case 'H': {
+          break;
+        }
+
+        case 'W': {
+          break;
+        }
+
+        case 'M': {
+          break;
+        }
+
+        case 'C': {
+          break;
+        }
+
+        case 'G': {
+          break;
+        }
+
+        default: {
+          break;
+        }
+      }
+    }
+  }
+
+  applyHover(event: Event) {
+
+    if (event.target !== this.currentSelection) {
+      if (this.currentHover) {
+        this.renderer.setStyle(this.currentHover, 'line-height', '40px');
+      }
+
+      this.currentSelection = event.target;
+      this.renderer.setStyle(this.currentHover, 'line-height', '25px');
+    }
   }
 
 }
+
